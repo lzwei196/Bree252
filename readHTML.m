e36138a0783d2e81320b8369608381c81243f432
SichenWan <35852165+SichenWan@users.noreply.github.com>
2018-11-24 17:43:11 -0500

    Add files via upload

---
+++ readHTML.m
@@ -0,0 +1,28 @@
+%Function that take district and number of page as input and return a vertor of
+%all kijiji pages with appartment posting
+function html = readHTML(district, numPage)
+
+%base url
+base_url = "https://www.kijiji.ca/";
+%apartment 
+apartment = "b-appartement-condo/";
+%search code
+code = "/c37l1700281";
+
+%pattern for pages 
+page = strings(1,numPage);
+    for i = 1:numPage
+        if i == 1 
+            page(i) = "";
+        else 
+            page(i) = strcat("/page-",string(i));
+        end
+    end
+disp(page);
+%concatenate sting to find all urls and read to html
+html = strings(1,numPage);
+    for i = 1:numPage
+        current_url = strcat(base_url,apartment,district,page(i),code);
+        html(i) = string(webread(current_url));
+    end
+end
